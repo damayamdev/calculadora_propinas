9dac0995d45d9caf55fcd13f8b237bcd61d2fdb3
damayamdev <164294520+damayamdev@users.noreply.github.com>
2024-04-03 19:17:39 -0500

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-
-
 function App() {
   return (
     <>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black uppercase">
+          Calculadora de Propinas y Consumo
+        </h1>
+      </header>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
